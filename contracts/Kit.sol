65e9ce9693d60e4e76266aa98f4886a82fe991b9
Brett Sun <qisheng.brett.sun@gmail.com>
2018-07-23 12:45:08 +0200

    Fix whitespace in Kit.sol

--- contracts/Kit.sol
+++ contracts/Kit.sol
@@ -20,18 +20,18 @@ contract KitBase is APMNamehash {
     event InstalledApp(address appProxy, bytes32 appId);
 
     function KitBase(DAOFactory _fac, ENS _ens) {
-    	ens = _ens;
+        ens = _ens;
 
-    	// If no factory is passed, get it from on-chain bare-kit
-    	if (address(_fac) == address(0)) {
-    		bytes32 bareKit = apmNamehash("bare-kit");
-    		fac = KitBase(latestVersionAppBase(bareKit)).fac();
-    	} else {
-    		fac = _fac;
-    	}
+        // If no factory is passed, get it from on-chain bare-kit
+        if (address(_fac) == address(0)) {
+            bytes32 bareKit = apmNamehash("bare-kit");
+            fac = KitBase(latestVersionAppBase(bareKit)).fac();
+        } else {
+            fac = _fac;
+        }
     }
 
-	function latestVersionAppBase(bytes32 appId) public view returns (address base) {
+    function latestVersionAppBase(bytes32 appId) public view returns (address base) {
         Repo repo = Repo(PublicResolver(ens.resolver(appId)).addr(appId));
         (,base,) = repo.getLatest();
 
@@ -40,54 +40,54 @@ contract KitBase is APMNamehash {
 }
 
 contract Kit is KitBase {
-	MiniMeTokenFactory tokenFactory;
+    MiniMeTokenFactory tokenFactory;
 
-	uint256 constant PCT = 10 ** 16;
-	address constant ANY_ENTITY = address(-1);
+    uint256 constant PCT = 10 ** 16;
+    address constant ANY_ENTITY = address(-1);
 
-	function Kit(ENS ens) KitBase(DAOFactory(0), ens) {
-		tokenFactory = new MiniMeTokenFactory();
-	}
+    function Kit(ENS ens) KitBase(DAOFactory(0), ens) {
+        tokenFactory = new MiniMeTokenFactory();
+    }
 
-	function newInstance() {
-		Kernel dao = fac.newDAO(this);
-		ACL acl = ACL(dao.acl());
-		acl.createPermission(this, dao, dao.APP_MANAGER_ROLE(), this);
+    function newInstance() {
+        Kernel dao = fac.newDAO(this);
+        ACL acl = ACL(dao.acl());
+        acl.createPermission(this, dao, dao.APP_MANAGER_ROLE(), this);
 
-		address root = msg.sender;
-		bytes32 appId = apmNamehash("app");
-		bytes32 votingAppId = apmNamehash("voting");
-		bytes32 tokenManagerAppId = apmNamehash("token-manager");
+        address root = msg.sender;
+        bytes32 appId = apmNamehash("app");
+        bytes32 votingAppId = apmNamehash("voting");
+        bytes32 tokenManagerAppId = apmNamehash("token-manager");
 
-		CounterApp app = CounterApp(dao.newAppInstance(appId, latestVersionAppBase(appId)));
-		Voting voting = Voting(dao.newAppInstance(votingAppId, latestVersionAppBase(votingAppId)));
-		TokenManager tokenManager = TokenManager(dao.newAppInstance(tokenManagerAppId, latestVersionAppBase(tokenManagerAppId)));
+        CounterApp app = CounterApp(dao.newAppInstance(appId, latestVersionAppBase(appId)));
+        Voting voting = Voting(dao.newAppInstance(votingAppId, latestVersionAppBase(votingAppId)));
+        TokenManager tokenManager = TokenManager(dao.newAppInstance(tokenManagerAppId, latestVersionAppBase(tokenManagerAppId)));
 
-		MiniMeToken token = tokenFactory.createCloneToken(address(0), 0, "App token", 0, "APP", true);
-		token.changeController(tokenManager);
+        MiniMeToken token = tokenFactory.createCloneToken(address(0), 0, "App token", 0, "APP", true);
+        token.changeController(tokenManager);
 
-		tokenManager.initialize(token, true, 0, true);
-		// Initialize apps
-		voting.initialize(token, 50 * PCT, 20 * PCT, 1 days);
+        tokenManager.initialize(token, true, 0, true);
+        // Initialize apps
+        voting.initialize(token, 50 * PCT, 20 * PCT, 1 days);
 
-		acl.createPermission(this, tokenManager, tokenManager.MINT_ROLE(), this);
-		tokenManager.mint(root, 1); // Give one token to root
+        acl.createPermission(this, tokenManager, tokenManager.MINT_ROLE(), this);
+        tokenManager.mint(root, 1); // Give one token to root
 
-		acl.createPermission(ANY_ENTITY, voting, voting.CREATE_VOTES_ROLE(), root);
+        acl.createPermission(ANY_ENTITY, voting, voting.CREATE_VOTES_ROLE(), root);
 
-		acl.createPermission(voting, app, app.INCREMENT_ROLE(), voting);
-		acl.createPermission(ANY_ENTITY, app, app.DECREMENT_ROLE(), root);
-		acl.grantPermission(voting, tokenManager, tokenManager.MINT_ROLE());
+        acl.createPermission(voting, app, app.INCREMENT_ROLE(), voting);
+        acl.createPermission(ANY_ENTITY, app, app.DECREMENT_ROLE(), root);
+        acl.grantPermission(voting, tokenManager, tokenManager.MINT_ROLE());
 
-		// Clean up permissions
-		acl.grantPermission(root, dao, dao.APP_MANAGER_ROLE());
-		acl.revokePermission(this, dao, dao.APP_MANAGER_ROLE());
-		acl.setPermissionManager(root, dao, dao.APP_MANAGER_ROLE());
+        // Clean up permissions
+        acl.grantPermission(root, dao, dao.APP_MANAGER_ROLE());
+        acl.revokePermission(this, dao, dao.APP_MANAGER_ROLE());
+        acl.setPermissionManager(root, dao, dao.APP_MANAGER_ROLE());
 
-		acl.grantPermission(root, acl, acl.CREATE_PERMISSIONS_ROLE());
-		acl.revokePermission(this, acl, acl.CREATE_PERMISSIONS_ROLE());
-		acl.setPermissionManager(root, acl, acl.CREATE_PERMISSIONS_ROLE());
+        acl.grantPermission(root, acl, acl.CREATE_PERMISSIONS_ROLE());
+        acl.revokePermission(this, acl, acl.CREATE_PERMISSIONS_ROLE());
+        acl.setPermissionManager(root, acl, acl.CREATE_PERMISSIONS_ROLE());
 
-		DeployInstance(dao);
-	}
+        DeployInstance(dao);
+    }
 }
